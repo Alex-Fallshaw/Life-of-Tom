0bfb087be3b87f2c2b74164857df4a37172a0aff
Alex Fallshaw <alex@fallshaw.me>
2019-06-03 18:09:36 +1000

    we kept doin the stuff and its not done

--- Game.py
+++ Game.py
@@ -1,13 +1,15 @@
 import json
 import sys
 
+
 class Game:
     def __init__(self, json_file_path):
         json_file = open(json_file_path)
         config = json.load(json_file)
+        self.store = Store()
         self.scenes = []
         for scene_config in config["scenes"]:
-            scene = Scene(scene_config)
+            scene = Scene(self.store, scene_config)
             self.scenes.append(scene)
         self.current_scene = self.scenes[0]
 
@@ -16,7 +18,7 @@ class Game:
             next_scene_id = self.current_scene.play()
             self.current_scene = self.get_scene(next_scene_id)
         print('Thanks for playing!')
-        
+
     def get_scene(self, next_scene_id):
         for scene in self.scenes:
             if scene.id == next_scene_id:
@@ -24,10 +26,10 @@ class Game:
 
 
 class Scene:
-    def __init__(self, scene_config):
-        self.config = scene_config
+    def __init__(self, store, scene_config):
+        self.store = store
         self.id = scene_config["id"]
-        self.prompt = Prompt(scene_config["prompt"])
+        self.prompt = Prompt(store, scene_config["prompt"])
         self.options = scene_config["options"]
 
     def play(self):
@@ -56,14 +58,27 @@ class Scene:
         next_scene_id = option["next_scene"]
         return next_scene_id
 
+
 class Prompt:
-    def __init__(self, prompt_config):
-        self.config = prompt_config
+    def __init__(self, store, prompt_config):
+        self.store = store
+        if type(prompt_config) is str:
+            # This section written by Alex's dad
+            self.prompt_parts = [
+                PromptPart(store, {
+                    "text_if_met": prompt_config,
+                    "text_if_not_met": '',
+                    "requirements": []
+                })
+            ]
+            # End part written by Alex's dad
+        else:
+            self.prompt_parts = []
+            for part_config in prompt_config:
+                part = PromptPart(store, part_config)
+                self.prompt_parts.append(part)
 
     def text(self):
-        if type(self.config) is str:
-            return self.config
-        else:
             full_prompt = ""
             for part_config in self.config:
                 full_prompt += self.part_text(part_config)
@@ -75,27 +90,46 @@ class Prompt:
         else:
             return part.text_if_not_met
 
+
 class PromptPart:
-    def __init__(self, part_config):
-        self.config = part_config
+    def __init__(self, store, part_config):
+        self.store = store
         self.text_if_met = part_config['text_if_met']
         self.text_if_not_met = part_config["text_if_not_met"]
-        self.requirements = part_config["requirements"]
+        self.requirements = []
+        for requirement_config in part_config["requirements"]:
+            requirement = Requirement(store, requirement_config)
+            self.requirements.append(requirement)
 
-    def self.has_met_requirements(self):
-        for requirement in requirements:
+    def has_met_requirements(self):
+        for requirement in self.requirements:
             if requirement.met():
+                return True
+
 
 class Requirement:
-    def __init__(self, requirement_config):
-        self.name = requirement_config['name']
-        self.value = requirement_config['value']
+    def __init__(self, store, requirement_config):
+        self.store = store
+        self.name = requirement_config["name"]
+        self.value = requirement_config["value"]
 
     def met(self):
         if self.store(self.name) == self.value:
             return True
         else:
             return False
-    
+
+
+class Store:
+    def __init__(self):
+        self.store = {}
+
+    def get(self, name):
+        return self.store[name]
+
+    def set(self, name, value):
+        self.store[name] = value
+
+
 game = Game('game.json')
 game.play()
